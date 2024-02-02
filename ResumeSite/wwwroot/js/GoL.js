b44eb8ed3798fdac6c6d1084e870ef3c2d977c3e
Atamosk <williamedwardkenney@gmail.com>
2024-02-01 19:55:42 -0500

    working on logic

--- ResumeSite/wwwroot/js/GoL.js
+++ ResumeSite/wwwroot/js/GoL.js
@@ -1,6 +1,14 @@
 var GameOfLife = /** @class */ (function () {
     function GameOfLife(height, width) {
         var _this = this;
+        this.lifeHappens = function (e) {
+            _this.running = true;
+            for (var j = 0; j < 1; j++) {
+                _this.deriveNextGen();
+                _this.drawAllTrue();
+            }
+            _this.running = false;
+        };
         this.pressEvent = function (e) {
             var eventX = e.changedTouches ?
                 e.changedTouches[0].pageX :
@@ -8,8 +16,8 @@ var GameOfLife = /** @class */ (function () {
             var eventY = e.changedTouches ?
                 e.changedTouches[0].clientY :
                 e.pageY;
-            var arrX = Math.floor(((eventX -= _this.canvas.offsetLeft) - 2) / 11);
-            var arrY = Math.floor(((eventY -= _this.canvas.offsetTop) - 2) / 11);
+            var arrX = Math.floor(((eventX - (_this.canvas.offsetLeft) - 2) + 11) / 11);
+            var arrY = Math.floor(((eventY - (_this.canvas.offsetTop) - 2) + 11) / 11);
             if (_this.currentGen[arrX][arrY] == true) {
                 _this.currentGen[arrX][arrY] = false;
             }
@@ -18,58 +26,112 @@ var GameOfLife = /** @class */ (function () {
             }
             _this.drawAllTrue();
         };
+        this.clearGrid = function (e) {
+            for (var x = 0; x < _this.width + 2; x++) {
+                _this.currentGen[x] = [];
+                for (var y = 0; y < _this.height + 2; y++) {
+                    _this.currentGen[x][y] = false;
+                }
+            }
+            _this.drawAllTrue();
+        };
         var canvas = document.getElementById('canvas');
         var context = canvas.getContext("2d");
+        var clearBtn = document.getElementById('clearBtn');
+        var playBtn = document.getElementById('playBtn');
         this.currentGen = [];
         this.nextGen = [];
         this.canvas = canvas;
         this.context = context;
+        this.clearBtn = clearBtn;
+        this.playBtn = playBtn;
         this.width = width;
         this.height = height;
         this.running = false;
-        for (var x = 0; x < this.width; x++) {
+        for (var x = 0; x < this.width + 2; x++) {
             this.nextGen[x] = [];
-            for (var y = 0; y < this.height; y++) {
+            for (var y = 0; y < this.height + 2; y++) {
                 this.nextGen[x][y] = false;
             }
         }
-        for (var x = 0; x < this.width; x++) {
+        for (var x = 0; x < this.width + 2; x++) {
             this.currentGen[x] = [];
-            for (var y = 0; y < this.height; y++) {
+            for (var y = 0; y < this.height + 2; y++) {
                 this.currentGen[x][y] = false;
             }
         }
         this.initUserInput();
         this.drawAllTrue();
     }
+    GameOfLife.prototype.deriveNextGen = function () {
+        var currentCell = false;
+        var neighbors = 0;
+        for (var x = 1; x <= this.width; x++) {
+            this.currentGen[x] = [];
+            for (var y = 1; y <= this.height; y++) {
+                if (this.currentGen[x - 1][y - 1]) {
+                    neighbors++;
+                }
+                if (this.currentGen[x - 1][y]) {
+                    neighbors++;
+                }
+                if (this.currentGen[x - 1][y + 1]) {
+                    neighbors++;
+                }
+                if (this.currentGen[x][y - 1]) {
+                    neighbors++;
+                }
+                if (this.currentGen[x][y + 1]) {
+                    neighbors++;
+                }
+                if (this.currentGen[x + 1][y - 1]) {
+                    neighbors++;
+                }
+                if (this.currentGen[x + 1][y]) {
+                    neighbors++;
+                }
+                if (this.currentGen[x + 1][y + 1]) {
+                    neighbors++;
+                }
+                if (this.currentGen[x][y] == false && neighbors == 3) {
+                    this.nextGen[x][y] = true;
+                }
+                else if (this.currentGen[x][y] == true && (neighbors < 2 || neighbors > 3)) {
+                    this.nextGen[x][y] = false;
+                }
+                neighbors = 0;
+            }
+        }
+        for (var x = 0; x < this.width + 2; x++) {
+            this.currentGen[x] = [];
+            for (var y = 0; y < this.height + 2; y++) {
+                this.currentGen[x][y] = this.nextGen[x][y];
+            }
+        }
+        this.drawAllTrue();
+    };
     GameOfLife.prototype.drawAllTrue = function () {
-        for (var x = 0; x < this.width; x++) {
-            for (var y = 0; y < this.height; y++) {
+        for (var x = 1; x <= this.width; x++) {
+            for (var y = 1; y <= this.height; y++) {
                 if (this.currentGen[x][y] == true) {
-                    this.context.fillRect((x * 10 + x + 1), (y * 10 + y + 1), 10, 10);
+                    this.context.fillRect((x * 10 + x + 1) - 11, (y * 10 + y + 1) - 11, 10, 10);
                 }
                 else if (this.currentGen[x][y] == false) {
-                    this.context.clearRect((x * 10 + x + 1), (y * 10 + y + 1), 10, 10);
+                    this.context.clearRect((x * 10 + x + 1) - 11, (y * 10 + y + 1) - 11, 10, 10);
                 }
             }
         }
     };
     GameOfLife.prototype.initUserInput = function () {
         var canvas = this.canvas;
+        var clearBtn = this.clearBtn;
+        var playBtn = this.playBtn;
         canvas.addEventListener("mousedown", this.pressEvent);
-        //canvas.addEventListener("mouseup", this.releaseEvent);
-        //canvas.addEventListener("mousemove", this.cancelEvent);
         canvas.addEventListener("touchstart", this.pressEvent);
-        //canvas.addEventListener("touchend", this.releaseEvent);
-        //canvas.addEventListener("touchcancel", this.cancelEvent);
-    };
-    GameOfLife.prototype.click = function (x, y, drag) {
-        this.clickX = x;
-        this.clickY = y;
-        this.clickDrag = drag;
+        clearBtn.addEventListener("click", this.clearGrid);
+        playBtn.addEventListener("click", this.lifeHappens);
     };
     return GameOfLife;
 }());
-;
 new GameOfLife(50, 50);
 //# sourceMappingURL=GoL.js.map
--- ResumeSite/wwwroot/js/GoL.ts
+++ ResumeSite/wwwroot/js/GoL.ts
@@ -9,41 +9,104 @@
     private clickX: number;
     private clickY: number;
     private clickDrag: boolean;
+    private clearBtn: HTMLButtonElement;
+    private playBtn: HTMLButtonElement;
 
     constructor(height: number, width: number) {
         let canvas = document.getElementById('canvas') as HTMLCanvasElement;
         let context = canvas.getContext("2d");
+        let clearBtn = document.getElementById('clearBtn') as HTMLButtonElement;
+        let playBtn = document.getElementById('playBtn') as HTMLButtonElement;
         this.currentGen = [];
         this.nextGen = [];
         this.canvas = canvas;
         this.context = context;
+        this.clearBtn = clearBtn;
+        this.playBtn = playBtn;
         this.width = width;
         this.height = height;
         this.running = false;
-        for (let x = 0; x < this.width; x++) {
+        for (let x = 0; x < this.width +2; x++) {
             this.nextGen[x] = [];
-            for (let y = 0; y < this.height; y++) {
+            for (let y = 0; y < this.height +2; y++) {
                 this.nextGen[x][y] = false;
             }
         }
-        for (let x = 0; x < this.width; x++) {
+        for (let x = 0; x < this.width + 2; x++) {
             this.currentGen[x] = [];
-            for (let y = 0; y < this.height; y++) {
+            for (let y = 0; y < this.height +2; y++) {
                 this.currentGen[x][y] = false;
             }
         }
         this.initUserInput();
         this.drawAllTrue();
     }
+    private lifeHappens = (e: Event) => {
+        this.running = true;
+        for (let j = 0; j < 1; j++) {
+            this.deriveNextGen();
+            this.drawAllTrue();
+        }
+        this.running = false;
+    }
+
+    private deriveNextGen() {
+        let currentCell = false;
+        let neighbors = 0;
+        for (let x = 1; x <= this.width; x++) {
+            this.currentGen[x] = [];
+            for (let y = 1; y <= this.height; y++) {
+                if (this.currentGen[x - 1][y - 1]) {
+                    neighbors++;
+                }
+                if (this.currentGen[x - 1][y]) {
+                    neighbors++;
+                }
+                if (this.currentGen[x - 1][y + 1]) {
+                    neighbors++;
+                }
+                if (this.currentGen[x][y - 1]) {
+                    neighbors++;
+                }
+                if (this.currentGen[x][y + 1]) {
+                    neighbors++;
+                }
+                if (this.currentGen[x + 1][y - 1]) {
+                    neighbors++;
+                }
+                if (this.currentGen[x + 1][y]) {
+                    neighbors++;
+                }
+                if (this.currentGen[x + 1][y + 1]) {
+                    neighbors++;
+                }
+                if (this.currentGen[x][y] == false && neighbors == 3) {
+                    this.nextGen[x][y] = true;
+                }
+                else if (this.currentGen[x][y] == true && (neighbors < 2 || neighbors > 3)) {
+                    this.nextGen[x][y] = false;
+                }
+                neighbors = 0;
+            }
+        }
+        for (let x = 0; x < this.width + 2; x++) {
+            this.currentGen[x] = [];
+            for (let y = 0; y < this.height + 2; y++) {
+                this.currentGen[x][y] = this.nextGen[x][y];
+            }
+        }
+        this.drawAllTrue();
+    }
+
 
     private drawAllTrue() {
-        for (let x = 0; x < this.width; x++) {
-            for (let y = 0; y < this.height; y++) {
+        for (let x = 1; x <= this.width; x++) {
+            for (let y = 1; y <= this.height; y++) {
                 if (this.currentGen[x][y] == true) {
-                    this.context.fillRect((x * 10 + x + 1), (y * 10 + y + 1), 10, 10);
+                    this.context.fillRect((x * 10 + x + 1) -11, (y * 10 + y + 1) - 11, 10, 10);
                 }
                 else if (this.currentGen[x][y] == false) {
-                    this.context.clearRect((x * 10 + x + 1), (y * 10 + y + 1), 10, 10);
+                    this.context.clearRect((x * 10 + x + 1) - 11, (y * 10 + y + 1) - 11, 10, 10);
                 }
             }
         }
@@ -51,20 +114,16 @@
 
     private initUserInput() {
         let canvas = this.canvas;
+        let clearBtn = this.clearBtn;
+        let playBtn = this.playBtn;
 
         canvas.addEventListener("mousedown", this.pressEvent);
-        //canvas.addEventListener("mouseup", this.releaseEvent);
-        //canvas.addEventListener("mousemove", this.cancelEvent);
 
         canvas.addEventListener("touchstart", this.pressEvent);
-        //canvas.addEventListener("touchend", this.releaseEvent);
-        //canvas.addEventListener("touchcancel", this.cancelEvent);
-    }
+        
+        clearBtn.addEventListener("click", this.clearGrid);
 
-    private click(x: number, y: number, drag: boolean) {
-        this.clickX = x;
-        this.clickY = y;
-        this.clickDrag = drag;
+        playBtn.addEventListener("click", this.lifeHappens);
     }
 
     private pressEvent = (e: MouseEvent | TouchEvent) => {
@@ -75,8 +134,8 @@
             (e as TouchEvent).changedTouches[0].clientY :
             (e as MouseEvent).pageY;
 
-        let arrX = Math.floor(((eventX -= this.canvas.offsetLeft) - 2)/ 11);
-        let arrY = Math.floor(((eventY -= this.canvas.offsetTop) - 2)/ 11);
+        let arrX = Math.floor(((eventX - (this.canvas.offsetLeft) - 2) + 11) / 11);
+        let arrY = Math.floor(((eventY - (this.canvas.offsetTop) - 2)  + 11) / 11);
         if (this.currentGen[arrX][arrY] == true) {
             this.currentGen[arrX][arrY] = false;
         }
@@ -86,6 +145,15 @@
         this.drawAllTrue();
 
     }
-};
+    private clearGrid = (e: Event) => {
+        for (let x = 0; x < this.width + 2; x++) {
+            this.currentGen[x] = [];
+            for (let y = 0; y < this.height + 2; y++) {
+                this.currentGen[x][y] = false;
+            }
+        }
+        this.drawAllTrue();
+    }
+}
 
 new GameOfLife(50, 50);
